4ccda25e836459f5d566ac55c29a0b97cb24b01e
chris <cjfiel322@gmail.com>
2026-01-08 20:52:50 +0800

    Formatted using prettier autoformatting

--- index.js
+++ index.js
@@ -9,51 +9,48 @@ const computerScoreDisplay = document.getElementById("computerScoreDisplay");
 let playerScore = 0;
 let computerScore = 0;
 
-function playGame(playerChoice){
+function playGame(playerChoice) {
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  let result = "";
 
-    const computerChoice = choices[Math.floor(Math.random() * 3)];
-    let result = "";
-
-    if(playerChoice === computerChoice){
-        result = "IT'S A TIE!";
-    }
-    else if (playerChoice === "mahoraga") {
-        result = "Mahoraga adapted \nYOU WIN!"
-        playerScore++;
-        playerScoreDisplay.textContent = playerScore;
-        resultDisplay.textContent = result;
-        resultDisplay.style.color = "green";
-    }
-    else{
-        switch(playerChoice){
-            case "rock":
-                result = (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!";
-                break;
-            case "paper":
-                result = (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!";
-                break;
-            case "scissors":
-                result = (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE!";
-                break;
-        }
-    }
-
-    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
-    computerDisplay.textContent = `Computer: ${computerChoice}`;
+  if (playerChoice === computerChoice) {
+    result = "IT'S A TIE!";
+  } else if (playerChoice === "mahoraga") {
+    result = "Mahoraga adapted \nYOU WIN!";
+    playerScore++;
+    playerScoreDisplay.textContent = playerScore;
     resultDisplay.textContent = result;
-
-    resultDisplay.classList.remove("greenText", "redText");
-
-    switch(result){
-        case "YOU WIN!":
-            resultDisplay.classList.add("greenText");
-            playerScore++;
-            playerScoreDisplay.textContent = playerScore;
-            break;
-        case "YOU LOSE!":
-            resultDisplay.classList.add("redText");
-            computerScore++;
-            computerScoreDisplay.textContent = computerScore;
-            break;
+    resultDisplay.style.color = "green";
+  } else {
+    switch (playerChoice) {
+      case "rock":
+        result = computerChoice === "scissors" ? "YOU WIN!" : "YOU LOSE!";
+        break;
+      case "paper":
+        result = computerChoice === "rock" ? "YOU WIN!" : "YOU LOSE!";
+        break;
+      case "scissors":
+        result = computerChoice === "paper" ? "YOU WIN!" : "YOU LOSE!";
+        break;
     }
+  }
+
+  playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+  computerDisplay.textContent = `Computer: ${computerChoice}`;
+  resultDisplay.textContent = result;
+
+  resultDisplay.classList.remove("greenText", "redText");
+
+  switch (result) {
+    case "YOU WIN!":
+      resultDisplay.classList.add("greenText");
+      playerScore++;
+      playerScoreDisplay.textContent = playerScore;
+      break;
+    case "YOU LOSE!":
+      resultDisplay.classList.add("redText");
+      computerScore++;
+      computerScoreDisplay.textContent = computerScore;
+      break;
+  }
 }
